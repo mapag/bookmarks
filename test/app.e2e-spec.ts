e6eff4aae32f053cd560dc4b4b323bf655f1f1f4
mapag <23513286+mapag@users.noreply.github.com>
2022-04-08 17:03:20 -0300

    feat: add e2e tests add auth suite add user get me test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,15 @@
+import { AuthDto } from './../src/auth/dto/auth.dto';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 
 import { AppModule } from './../src/app.module';
 import { PrismaService } from './../src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('App e2e', () => {
   let app: INestApplication;
   let prisma: PrismaService;
-
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -18,15 +20,137 @@ describe('App e2e', () => {
         whitelist: true,
       }),
     );
+    const configService = app.get<ConfigService>(ConfigService);
+
     await app.init();
+    await app.listen(configService.get('port'));
 
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('pass');
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'test@test.com',
+      password: '123',
+    };
+    describe('Sign up', () => {
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if no body', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+
+      it('should sign up', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+
+      it('should throw if email exists', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(403);
+      });
+    });
+
+    describe('Sign in', () => {
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if no body', () => {
+        return pactum.spec().post('/auth/signin').expectStatus(400);
+      });
+
+      it('should sign in', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
+      });
+    });
+  });
+
+  describe('User', () => {
+    const userEmail = 'test@test.com';
+    describe('Get me', () => {
+      it.todo('Should pass');
+      it('Should current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders('Authorization', 'Bearer $S{userAt}')
+          .expectStatus(200)
+          .expectBodyContains(userEmail);
+      });
+    });
+    describe('Edit user', () => {
+      it.todo('Should pass');
+    });
+  });
+
+  describe('Bookmarks', () => {
+    describe('Get bookmarks', () => {
+      it.todo('Should pass');
+    });
+    describe('Add bookmarks', () => {
+      it.todo('Should pass');
+    });
+    describe('Get bookmarks by id', () => {
+      it.todo('Should pass');
+    });
+    describe('Edit bookmarks', () => {
+      it.todo('Should pass');
+    });
+    describe('Delete bookmarks', () => {
+      it.todo('Should pass');
+    });
+  });
 });
